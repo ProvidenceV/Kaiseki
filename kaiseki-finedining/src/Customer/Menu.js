9b99f83565fac59f1014f2d86af461f2f2b6940a
Alexander Grisby <grisbyar@mail.uc.edu>
2022-12-04 22:32:13 -0500

    fixed video finally

--- kaiseki-finedining/src/Customer/Menu.js
+++ kaiseki-finedining/src/Customer/Menu.js
@@ -1,9 +1,25 @@
 export default function Menu(){
     return(
         <>
-        <div>
+        <div className="menu">
+          <div className="menu-sections">
+            <div className="menu-section-drinks">
+                <h1>Drinks</h1>
+
+            </div>
+            <div className="menu-section-starters">
+              <h1>starters</h1>
+            </div>
+            <div className="menu-section-main">
+              <h1>Main</h1>
+            </div>
+            <div className="menu-section-deserts">
+              <h1>Dessert</h1>
+            </div>
+          </div>
             <p>Menu goes here</p>
         </div>
         </>
     )
 }
+
