2fb952b1689aba17d0843917db9854802bfd7566
Alexander Grisby <grisbyar@mail.uc.edu>
2022-12-05 01:23:32 -0500

    menu updates

--- kaiseki-finedining/src/Customer/Menu.js
+++ kaiseki-finedining/src/Customer/Menu.js
@@ -1,4 +1,15 @@
+import React from 'react'
+
 export default function Menu(){
+//get all images
+  function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  }
+  
+  const images = importAll(require.context('../assets/images', false, /\.(png|jpe?g|svg)$/));
+
     return(
         <>
         <div className="menu">
@@ -6,156 +17,156 @@ export default function Menu(){
           <hr className="break-view" />
             <div className="menu-section-drinks">
                   <h1>Drinks</h1>
-                  <div class="menu-row">
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                  <div className="menu-row">
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                       <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                       <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                    </div>
                   </div>
-                  <div class="menu-row">
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                  <div className="menu-row">
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                       <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                       <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                    </div>
                   </div>
             </div>
             <hr className="break-view" />
             <div className="menu-section-starters">
-              <h1>starters</h1>
-              <div class="menu-row">
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+              <h1 className="menu-section-starters-header">starters</h1>
+              <div className="menu-row">
+                    <div className="menu-column">
+                      <div className="menu-card"><img className="menu-images" alt="starter1" src={images['starter1.jpg']} /><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><img className="menu-images" alt="starter2" src={images['starter2.jpg']} /><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                       <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                       <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                    </div>
                   </div>
-                  <div class="menu-row">
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                  <div className="menu-row">
+                    <div className="menu-column">
+                      <div className="menu-card"><img className="menu-images" alt="starter3" src={images['starter3.jpg']} /><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                       <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                       <div className="menu-card"><img className="menu-images" alt="starter4" src={images['starter4.jpg']} /><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                    </div>
                   </div>
             </div>
             <hr className="break-view" />
             <div className="menu-section-main">
-              <h1>Main</h1>
-              <div class="menu-row">
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+              <h1 className="menu-section-main-header">Main</h1>
+              <div className="menu-row">
+                    <div className="menu-column">
+                      <div className="menu-card"><img className="menu-images" alt="food1" src={images['food1.jpg']} /><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                       <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                       <div className="menu-card"><img className="menu-images" alt="food2" src={images['food2.jpg']} /><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                    </div>
                   </div>
-                  <div class="menu-row">
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                  <div className="menu-row">
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><img className="menu-images" alt="food3" src={images['food3.jpg']} /><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                       <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                       <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><img className="menu-images" alt="food4" src={images['food4.jpg']} /><p>stuff here</p></div>
                    </div>
                   </div>
-                  <div class="menu-row">
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                  <div className="menu-row">
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><img className="menu-images" alt="food5" src={images['food5.jpg']} /><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                       <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                       <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><img className="menu-images" alt="food6" src={images['food6.jpg']} /><p>stuff here</p></div>
                    </div>
                   </div>
-                  <div class="menu-row">
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                  <div className="menu-row">
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><img className="menu-images" alt="food7" src={images['food7.jpg']} /><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                       <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                       <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                    </div>
                   </div>
             </div>
             <hr className="break-view" />
-            <div className="menu-section-deserts">
-              <h1>Dessert</h1>
-              <div class="menu-row">
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+            <div className="menu-section-desserts">
+              <h1 className="menu-section-desserts-header">Dessert</h1>
+              <div className="menu-row">
+                    <div className="menu-column">
+                      <div className="menu-card"><img className="menu-images" alt="dessert1" src={images['dessert1.jpg']} /><p>stuff here</p><button>Add to order</button></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><img className="menu-images" alt="dessert2" src={images['dessert2.jpg']} /><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                       <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                       <div className="menu-card"><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><img className="menu-images" alt="dessert3" src={images['dessert3.jpg']} /><p>stuff here</p></div>
                    </div>
                   </div>
-                  <div class="menu-row">
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                  <div className="menu-row">
+                    <div className="menu-column">
+                      <div className="menu-card"><img className="menu-images" alt="dessert4" src={images['dessert4.jpg']} /><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><img className="menu-images" alt="dessert5" src={images['dessert5.jpg']} /><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                       <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                       <div className="menu-card"><img className="menu-images" alt="dessert6" src={images['dessert6.jpg']} /><p>stuff here</p></div>
                     </div>
-                    <div class="menu-column">
-                      <div class="menu-card"><p>stuff here</p></div>
+                    <div className="menu-column">
+                      <div className="menu-card"><p>stuff here</p></div>
                    </div>
                   </div>
             </div>
--- kaiseki-finedining/src/Footer.js
+++ kaiseki-finedining/src/Footer.js
@@ -4,11 +4,28 @@ const Footer = () => {
     return(
         <>
         <footer>
-            <div className="footer">
-            <h4>
-                Contact us at 555-555-5555
-                123 Imaginary Place Way, XX, USA 12345
-            </h4>
+            <div className="main-footer">
+                <div className="footer-container">
+                    <div className="footer-row">
+                        <div className="footer-col">
+                         <h4>
+                            Contact us at 555-555-5555
+                            123 Imaginary Place Way, XX, USA 12345
+                        </h4>
+                        </div>
+                        <div className="footer-col">
+                        </div>
+                        <div className="footer-col">
+                         <h4>
+                            Other information here
+                        </h4>
+                        </div>
+                    </div>
+                </div>
+            <hr className="break-view"/>
+            <div>
+                <p>{new Date().getFullYear()} Kaiseki Ltd | All Rights Reserved | Terms of Service | Privacy</p>
+            </div>
             </div>
         </footer>
         </>
