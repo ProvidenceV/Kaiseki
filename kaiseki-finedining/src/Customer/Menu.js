f1a1f149d7f39f46ad63a861b78cc7886d53fe5d
Alexander Grisby <grisbyar@mail.uc.edu>
2022-12-04 23:48:17 -0500

    added more stuff

--- kaiseki-finedining/src/Customer/Menu.js
+++ kaiseki-finedining/src/Customer/Menu.js
@@ -3,20 +3,37 @@ export default function Menu(){
         <>
         <div className="menu">
           <div className="menu-sections">
+          <hr className="break-view" />
             <div className="menu-section-drinks">
-                <h1>Drinks</h1>
+                  <h1>Drinks</h1>
+                  <div class="menu-row">
+                    <div class="menu-column">
+                      <div class="menu-card"><p>stuff here</p></div>
+                    </div>
+                    <div class="menu-column">
+                      <div class="menu-card"><p>stuff here</p></div>
+                    </div>
+                    <div class="menu-column">
+                       <div class="menu-card"><p>stuff here</p></div>
+                    </div>
+                    <div class="menu-column">
+                      <div class="menu-card"><p>stuff here</p></div>
+                   </div>
+                  </div>
             </div>
+            <hr className="break-view" />
             <div className="menu-section-starters">
               <h1>starters</h1>
             </div>
+            <hr className="break-view" />
             <div className="menu-section-main">
               <h1>Main</h1>
             </div>
+            <hr className="break-view" />
             <div className="menu-section-deserts">
               <h1>Dessert</h1>
             </div>
           </div>
-            <p>Menu goes here</p>
         </div>
         </>
     )
