2bb17cab52b87b95d888cb5218cbe2b7ea7f73e9
Alexander Grisby <grisbyar@mail.uc.edu>
2022-12-05 19:54:03 -0500

    fixed navbar state use

--- kaiseki-finedining/src/Navbar.js
+++ kaiseki-finedining/src/Navbar.js
@@ -1,36 +1,28 @@
 import CustomerNav from "./Customer/CustomerNav"
 import RestaurauntNav from "./Restauraunt/RestaurauntNav"
 import "./navbarstyle.css"
-
+import {useState} from "react"
 
 
 function Navbar(){
-    const state = {
-        view: "customer"
-    };
+    const [view, setView] = useState("customer");
+
     let navType;  
-   if(state.view === "customer"){
+   if(view === "customer"){
         navType = <CustomerNav />;
-   }else if(state.view === "restauraunt"){
+   }else if(view === "restauraunt"){
         navType = <RestaurauntNav />;
    }
-//    //why doesn't this update
-//    if(window.location.pathname === "Restauraunt"){
-//         navType = <RestaurauntNav />;
-//    }
-//    else if(window.location.pathname === "Customer"){
-//         navType = <CustomerNav />;
-//    }
 
     return(
         <>
         <nav className="nav">
             <div className="view-wrapper">
                 <div className="view-customer">
-                    <p id="customer" onClick={() => {state.view = "customer"}}>Customer</p>
+                    <p id="customer" onClick={() => {setView("customer")}}>Customer</p>
                 </div>
                 <div className="view-restauraunt">
-                    <p id="restauraunt" onClick={() => {state.view = "restauraunt"}}>Restauraunt</p>
+                    <p id="restauraunt" onClick={() => {setView("restauraunt")}}>Restauraunt</p>
                 </div>
             </div>
             <hr className="break-view" />
